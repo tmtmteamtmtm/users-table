973c8b8563bb79213fc700579313097bfdb68c83
Songchai Panich <songchaiza02@gmail.com>
2023-05-18 17:17:41 +0700

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const url = 'https://dummyjson.com/users'
 
 function App() {
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      setData(res.data);
+    });
+  }, []);
+
+  if (!data) {
+    return <h1>Loading...</h1>;
+  }
+
+  console.log(data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+      {data.users.map((val) => (
+
+<table>
+<thead>
+  <tr>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>Ages</th>
+  </tr>
+</thead>
+<tbody>
+  {data.users.map((val) => (
+    <tr key={val.id}>
+      <td>{val.firstName}</td>
+      <td>{val.lastName}</td>
+      <td>{val.age}</td>
+    </tr>
+  ))}
+</tbody>
+</table>
+      ))}
     </div>
   );
 }
