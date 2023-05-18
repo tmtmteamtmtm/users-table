1483083c5482c1750f5b37c978c9bcecc0b7afef
Songchai Panich <songchaiza02@gmail.com>
2023-05-18 17:56:31 +0700

    main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import './App.css';
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const url = 'https://dummyjson.com/users'
+const url = "https://dummyjson.com/users";
 
 function App() {
-
-  const [data, setData] = useState();
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     axios.get(url).then((res) => {
@@ -18,30 +17,29 @@ function App() {
     return <h1>Loading...</h1>;
   }
 
-  console.log(data)
+  console.log(data);
 
   return (
-      <div className="App">
+    <div className="App">
       {data.users.map((val) => (
-
-<table>
-<thead>
-  <tr>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>Ages</th>
-  </tr>
-</thead>
-<tbody>
-  {data.users.map((val) => (
-    <tr key={val.id}>
-      <td>{val.firstName}</td>
-      <td>{val.lastName}</td>
-      <td>{val.age}</td>
-    </tr>
-  ))}
-</tbody>
-</table>
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Ages</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.users.map((val) => (
+              <tr key={val.id}>
+                <td>{val.firstName}</td>
+                <td>{val.lastName}</td>
+                <td>{val.age}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       ))}
     </div>
   );
